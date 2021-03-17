f7c029ea54ace9171902d29f133d458551b56d74
hyeseong-lee <hyeseong43@gmail.com>
2021-03-17 16:05:56 +0900

    Update user,product,order App Models - 일시 : 2021.03.17.16:05(수) - 내용 : 3개 app의 models.py파일 수정

--- order/models.py
+++ order/models.py
@@ -1,42 +1,40 @@
 from django.db import models
-from datetime import date
 
 class Order(models.Model):
-    user          = models.ForeignKey('user.User',    on_delete=models.CASCADE, related_name='orders')
-    order_status  = models.BooleanField(default=True)
-    create_at     = models.DateField(auto_now_add=True)
-    
-    class Meta:
-      db_table = 'orders'
+    user            = models.ForeignKey('user.user', on_delete=models.CASCADE)
+    status          = models.ForeignKey('order.status', on_delete=models.CASCADE)
+    address         = models.ForeignKey('user.address', on_delete=models.CASCADE, null=True)
+    product         = models.ManyToManyField('product.product', through='cart', related_name='shopping_cart')
+    create_at       = models.DateTimeField(auto_now_add=True)
+    total_price     = models.DecimalField(max_digits=12, decimal_places=2, default=0)
+    is_confirmed    = models.BooleanField(default=False)
+    serial_number   = models.CharField(max_length=50)
 
+    class Meta:
+        db_table = 'orders'
 
 class OrderStatus(models.Model):
-    status    = models.CharField(max_length=45)
-
-    class Meta:
-      db_table = 'order_status'
-      
-
-class OrderItem(models.Model): # Cart 개념
-    user      = models.ForeignKey('user.User',
-                                  on_delete=models.CASCADE,
-                                  related_name='order_items')
-    product   = models.ForeignKey('product.Product', 
-                                    on_delete=models.CASCADE, 
-                                    related_name='order_items')
-    order     = models.ForeignKey('Order',           
-                                    on_delete=models.CASCADE, 
-                                    related_name='order_items', 
-                                    null=True, blank=True)
-    quantity  = models.IntegerField(default=1)
-    create_at = models.DateField(auto_now_add=True)
-    class Meta:
-      db_table = 'order_items'
-
-    def sub_total(self):
-      return self.product.price * self.quantity
+    name        = models.CharField(max_length=20)
 
     def __str__(self):
-      return self.product
-    
-    
+        return f'{self.name}'
+
+    class Meta:
+        db_table = 'order_statuses'
+
+class Cart(models.Model):
+    order       = models.ForeignKey('order.order', on_delete=models.CASCADE)
+    product     = models.ForeignKey('product.product', on_delete=models.CASCADE)
+    option      = models.CharField(max_length=100, null=True)
+    quantity    = models.SmallIntegerField()
+    total_price = models.DecimalField(max_digits=8, decimal_places=2, default=0)
+    status      = models.ForeignKey('order.cartstatus', on_delete=models.CASCADE, default=1)
+
+    class Meta:
+        db_table = 'carts'
+
+class CartStatus(models.Model):
+    name = models.CharField(max_length=200)
+
+    class Meta:
+        db_table = 'cart_statuses'
