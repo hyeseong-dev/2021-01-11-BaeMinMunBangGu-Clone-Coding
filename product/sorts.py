2790a7236a5a4e068b163b7ddd865beb81e68280
HyeSeong <hyeseong43@gmail.com>
2021-04-18 13:54:31 +0900

    Update sorts.py

--- product/sorts.py
+++ product/sorts.py
@@ -5,10 +5,7 @@ def is_new(create_at, compare_date):
 
 def check_best_items():
     best_items = Product.objects.all().order_by('total_sales')[:20]
-    list_ = []
-    for best in best_items:
-        list_.append(best.id)
-    return list_
+    return [best.id for best in best_items]
 
 def is_best(checkList, arg):
     return arg in checkList 
